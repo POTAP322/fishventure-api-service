b75cfbdda2ab0651cb9a354bde65815940c60115
Potap <dragonkingmore@gmail.com>
2025-06-04 18:20:48 +0300

    feat(FISH-110): для реализации pipeline сделал, чтобы в конфиге ключи подключались из .env , добавил скрипт для деплоя на сервере

--- api/v1/config.py
+++ api/v1/config.py
@@ -2,13 +2,16 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Fishventure API"
-    SECRET_KEY: str = "your-secret-key"
-    ALGORITHM: str = "HS256"
+    SECRET_KEY: str
+    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 3000
-    DATABASE_URL: str = "mysql+pymysql://root:gh245hyt@localhost/fishventure_db"
-    QWEN_API_KEY: str = "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"
-    QWEN_API_URL: str = "https://openrouter.ai/api/v1"
-    QWEN_MODEL: str = "qwen/qwen3-235b-a22b:free"
+
+    DATABASE_URL: str
+    TEST_DATABASE_URL: str
+
+    QWEN_API_KEY: str
+    QWEN_API_URL: str
+    QWEN_MODEL: str
 
 
     class Config:
